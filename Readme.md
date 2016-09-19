272d246f93297f1c5265ff30c757562a9933fd6e
Michael Freeman <michael.k.freeman@gmail.com>
2016-09-19 16:12:20 -0700

    Init commit of starter files

---
+++ Readme.md
@@ -0,0 +1,1 @@
+# Starter repo for assignment 3
---
+++ js/map.js
@@ -0,0 +1,36 @@
+// Function to draw your map
+var drawMap = function() {
+
+    // Create map and set view
+
+
+    // Create a tile layer variable using the appropriate url
+
+
+    // Add the layer to your map
+
+
+    // Execute your function to get data
+
+};
+
+// Function for getting data
+var getData = function() {
+
+    // Execute an AJAX request to get the data in data/data.csv file
+
+    // Use the PapaParse library to parse the information returned by your request
+
+    // When your request is successful, call your customBuild function
+
+};
+
+// Loop through your data and add the appropriate layers and points
+var customBuild = function() {
+    // Be sure to add each layer to the map
+
+    // Once layers are on the map, add a leaflet controller that shows/hides layers
+
+    // Build a table showing calculated aggregate values
+
+};
